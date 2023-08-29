c56a1fe4546d9ef68edfa8f154b1325b28a43881
Irlan Farias <80763521+IrlanAndrade@users.noreply.github.com>
2023-08-29 19:03:37 -0300

    API para Blogs e Notícias
    Aqui vai a API de blogs e notícias para ser testado com o frontend, o arquivo bd.py funciona como banco de dados temporário, por sua vez não é atualizado, e é reiniciado a cada execução do programa para o valor default

---
+++ blog/bd.py
@@ -0,0 +1,17 @@
+noticias = [
+    {
+        'id': 1,
+        'titulo': 'Teste Titulo',
+        'descricao': 'Lorem ipsum dolor sit amet. Sit similique eligendi sit atque quis qui earum consequuntur aut provident aliquid. Aut repellendus voluptatibus nam soluta vero et molestias provident qui quisquam tenetur. Ut quibusdam nesciunt eos ducimus velit aut numquam ipsam qui dolorum commodi est voluptas voluptas! Ut architecto internos qui inventore tempora sed enim nisi id dolorem ipsam et consequatur quia et fugit voluptate.' 
+    },
+    {
+        'id': 2,
+        'titulo': 'Teste Titulo 2',
+        'descricao': 'Lorem ipsum dolor sit amet. Sit similique eligendi sit atque quis qui earum consequuntur aut provident aliquid. Aut repellendus voluptatibus nam soluta vero et molestias provident qui quisquam tenetur. Ut quibusdam nesciunt eos ducimus velit aut numquam ipsam qui dolorum commodi est voluptas voluptas! Ut architecto internos qui inventore tempora sed enim nisi id dolorem ipsam et consequatur quia et fugit voluptate.' 
+    },
+    {
+        'id': 3,
+        'titulo':'Teste Título 3',
+        'descricao':'Lorem ipsum dolor sit amet. Sit similique eligendi sit atque quis qui earum consequuntur aut provident aliquid. Aut repellendus voluptatibus nam soluta vero et molestias provident qui quisquam tenetur. Ut quibusdam nesciunt eos ducimus velit aut numquam ipsam qui dolorum commodi est voluptas voluptas! Ut architecto internos qui inventore tempora sed enim nisi id dolorem ipsam et consequatur quia et fugit voluptate.'
+    }
+]
---
+++ blog/main.py
@@ -0,0 +1,49 @@
+from flask import Flask, jsonify, request, make_response
+from bd import noticias
+
+app = Flask(__name__)
+
+# Consulta todas as notícias
+@app.route('localhost/noticias', methods=['GET'])
+def obterNoticias():
+    return jsonify(noticias)
+
+# Deleta notícia por ID
+@app.route('localhost/noticias/<int:id>', methods=['DELETE'])
+def excluirNoticia(id):
+    for i, noticia in enumerate(noticias):
+        if noticia.get('id') == id:
+            del noticias[i]
+            return jsonify(noticia)
+
+# Atualiza notícia por ID
+@app.route('localhost/noticias/<int:id>', methods=['PUT'])
+def editarNoticia(id):
+    noticiaEditada = request.get_json()
+    for i, noticia in enumerate(noticias):
+        print(i, noticia.get('id'))
+        if noticia.get('id') == id:
+            noticias[i].update(noticiaEditada)
+            return jsonify(noticiaEditada)
+
+# Cria notícia por ID
+@app.route('localhost/noticias', methods=['POST'])
+def criarNoticia():
+    pegaJson = request.get_json()
+    id = autoIncrementarId()
+    pegaJson["id"] = id
+    noticias.append(pegaJson)
+    return pegaJson
+
+def autoIncrementarId():
+    i = 1
+    if noticias:
+        for noticia in noticias:
+            if noticia.get('id') != i: 
+                return i
+            else:
+                i += 1
+    return i
+    
+app.run(port=9090)
+
